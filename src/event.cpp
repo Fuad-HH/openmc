7336449717c4f6aa5c1740e351b5d88108a537ae
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2024-11-28 18:35:21 -0500

    refactor process_advance_particle_events for loop

--- src/event.cpp
+++ src/event.cpp
@@ -103,15 +103,25 @@ void process_calculate_xs_events(SharedArray<EventQueueItem>& queue)
   simulation::time_event_calculate_xs.stop();
 }
 
-void process_advance_particle_events()
+void openmc_event_advance_wrapper()
 {
-  simulation::time_event_advance_particle.start();
-
 #pragma omp parallel for schedule(runtime)
   for (int64_t i = 0; i < simulation::advance_particle_queue.size(); i++) {
     int64_t buffer_idx = simulation::advance_particle_queue[i].idx;
     Particle& p = simulation::particles[buffer_idx];
     p.event_advance();
+  }
+}
+
+void process_advance_particle_events()
+{
+  simulation::time_event_advance_particle.start();
+  openmc_event_advance_wrapper();
+
+#pragma omp parallel for schedule(runtime)
+  for (int64_t i = 0; i < simulation::advance_particle_queue.size(); i++) {
+    int64_t buffer_idx = simulation::advance_particle_queue[i].idx;
+    Particle& p = simulation::particles[buffer_idx];
     if (!p.alive())
       continue;
     if (p.collision_distance() > p.boundary().distance) {
