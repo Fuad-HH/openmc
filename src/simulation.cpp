08b89db85fb36dfa59b2824983de1e93f4bda05e
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2025-03-21 17:17:53 -0400

    fix cuda deallocation error

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -203,6 +203,8 @@ int openmc_simulation_finalize()
     settings::p_pumi_tally->write_pumi_tally_mesh();
     printf("[PUMIPIC COPY TIME] PumiPic took %f seconds to copy positions\n",
       settings::particle_location_copy_time);
+
+    settings::p_pumi_tally.reset(nullptr);
   }
 #endif
 
